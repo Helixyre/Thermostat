e988814e86562b350af161c9708d1b63dc36e79b
Helixyre <leavictoria.c71@gmail.com>
2025-10-01 20:16:28 +0200

    Refactor Thermostat TP3

--- main.cpp
+++ main.cpp
@@ -15,6 +15,105 @@ float count1 = 0.0;
 float count2 = 0.0;
 float count3 = 0.0;
 
+void page0()
+{
+	//sprintf(txt, "TempH ");
+	sprintf(txt,"0");
+	LCD.locate(0, 1);
+	LCD.puts(txt);
+	sprintf(txt, "Degree: %1f ",count1);
+	LCD.locate(0, 0);
+	LCD.puts(txt);
+	if(S1.read() == 0)
+	{
+		thread_sleep_for(100);
+		while(S1.read() == 0)
+		{
+		}
+		if(count1 < 30.0)
+		{
+			count1 += 0.5;
+		}
+	}
+	if(S2.read() == 0)
+	{
+		thread_sleep_for(100);
+		while(S2.read() == 0)
+		{
+		}
+		if(count1 > -5.0)
+		{
+			count1 -= 0.5;
+		}
+	}
+}
+
+void page1()
+{
+	//sprintf(txt, "TempL ");
+	sprintf(txt,"1");
+	LCD.locate(0, 1);
+	LCD.puts(txt);
+	sprintf(txt, "Degree: %1f ",count2);
+	LCD.locate(0, 0);
+	LCD.puts(txt);
+	if(S1.read() == 0)
+	{
+		thread_sleep_for(100);
+		while(S1.read() == 0)
+		{
+		}
+		if(count2 < 20.0)
+		{
+			count2 += 0.5;
+		}
+	}
+	if(S2.read() == 0)
+	{
+		thread_sleep_for(100);
+		while(S2.read() == 0)
+		{
+		}
+		if(count2 > -10.0)
+		{
+			count2 -= 0.5;
+		}
+	}
+}
+
+void page2()
+{
+	//sprintf(txt, "TempAI");
+	sprintf(txt,"2");
+	LCD.locate(0, 1);
+	LCD.puts(txt);
+	sprintf(txt, "Degree: %1f ",count3);
+	LCD.locate(0, 0);
+	LCD.puts(txt);
+	if(S1.read() == 0)
+	{
+		thread_sleep_for(100);
+		while(S1.read() == 0)
+		{
+		}
+		if(count3 < 30.0)
+		{
+			count3 += 0.5;
+		}
+	}
+	if(S2.read() == 0)
+	{
+		thread_sleep_for(100);
+		while(S2.read() == 0)
+		{
+		}
+		if(count3 > -15.5)
+		{
+			count3 -= 0.5;
+		}
+	}
+}
+
 int main()
 {
 	thread_sleep_for(100);
@@ -24,122 +123,35 @@ int main()
 
 	while(1)
 	{
-		val = ADC0.read();
-		sprintf(txt, "%5.3f", val);
-		LCD.locate(7, 1);
-		LCD.puts(txt);
-		thread_sleep_for(100);
 		if(S3.read() == 0)
 		{
 			thread_sleep_for(100);
 			while(S3.read() == 0)
 			{
 			}
-			if(page < 2)
-			{
-				page += 1;
-			}
-			else
+			page++;
+			if(page > 2)
 			{
 				page = 0;
 			}
+			LCD.cls();
 		}
-		if(page == 0)
+		switch(page)
 		{
-			//sprintf(txt, "TempH ");
-			sprintf(txt,"0");
-			LCD.locate(0, 1);
-			LCD.puts(txt);
-			sprintf(txt, "Degree: %1f ",count1);
-			LCD.locate(0, 0);
-			LCD.puts(txt);
-			if(S1.read() == 0)
-			{
-				thread_sleep_for(100);
-				while(S1.read() == 0)
-				{
-				}
-				if(count1 < 30.0)
-				{
-					count1 += 0.5;
-				}
-			}
-			if(S2.read() == 0)
-			{
-				thread_sleep_for(100);
-				while(S2.read() == 0)
-				{
-				}
-				if(count1 > -5.0)
-				{
-					count1 -= 0.5;
-				}
-			}
+			case 0:
+				page0();
+				break;
+			case 1:
+				page1();
+				break;
+			case 2:
+				page2();
+				break;
+			default:
+				page = 0;
+				break;
 		}
-			if(page == 1)
-		{
-			//sprintf(txt, "TempL ");
-			sprintf(txt,"1");
-			LCD.locate(0, 1);
-			LCD.puts(txt);
-			sprintf(txt, "Degree: %1f ",count2);
-			LCD.locate(0, 0);
-			LCD.puts(txt);
-			if(S1.read() == 0)
-			{
-				thread_sleep_for(100);
-				while(S1.read() == 0)
-				{
-				}
-				if(count2 < 20.0)
-				{
-					count2 += 0.5;
-				}
-			}
-			if(S2.read() == 0)
-			{
-				thread_sleep_for(100);
-				while(S2.read() == 0)
-				{
-				}
-				if(count2 > -10.0)
-				{
-					count2 -= 0.5;
-				}
-			}
-		}
-			if(page == 2)
-		{
-			//sprintf(txt, "TempAI");
-			sprintf(txt,"2");
-			LCD.locate(0, 1);
-			LCD.puts(txt);
-			sprintf(txt, "Degree: %1f ",count3);
-			LCD.locate(0, 0);
-			LCD.puts(txt);
-			if(S1.read() == 0)
-			{
-				thread_sleep_for(100);
-				while(S1.read() == 0)
-				{
-				}
-				if(count3 < 30.0)
-				{
-					count3 += 0.5;
-				}
-			}
-			if(S2.read() == 0)
-			{
-				thread_sleep_for(100);
-				while(S2.read() == 0)
-				{
-				}
-				if(count3 > -15.5)
-				{
-					count3 -= 0.5;
-				}
-			}
-		}
+		thread_sleep_for(100);
 
 	}
 }
